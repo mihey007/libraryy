700149b2b5eb360dc1bb1cce79a52459bdb98b38
mihey007 <m.shepelev@ukr.net>
2013-05-17 10:46:30 +0300

    Update

---
+++ mihey.h
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+class Library{
+private:
+  string autherOfBook;
+	string nameOfBook;
+	int numberOfBook;
+public:
+	/*Library();	*/
+void DisplayLibrary();
+
+    int getLibrary();
+	
+	void setLibrary(string , string ,int );
+	
+
+};
+ 
